1ccbe94ca9ba6506660199bcb27c17eea69d09fd
JohnWeeks1 <jonathanweeks007@gmail.com>
2023-04-21 14:53:43 +0700

    first commit

--- components/MenuOverlay.vue
+++ components/MenuOverlay.vue
@@ -90,8 +90,29 @@
                     "
                 >
                     <div class=" flex items-center text-[20px] font-semibold">
-                        <Icon name="ph:pen-light" size="33"/>
+                        <Icon name="ph:sign-out-light" size="33"/>
                         <span class="pl-4">Sign out</span>
+                    </div>
+                </li>
+
+                <li 
+                    v-else 
+                    @click="signIn()"
+                    class="
+                        relative 
+                        flex 
+                        items-center 
+                        justify-between 
+                        py-2.5 
+                        border-b 
+                        px-3 
+                        hover:bg-gray-100 
+                        cursor-pointer
+                    "
+                >
+                    <div class=" flex items-center text-[20px] font-semibold">
+                        <Icon name="ph:sign-in-light" size="33"/>
+                        <span class="pl-4">Sign in / Register</span>
                     </div>
                 </li>
             </ul>
@@ -115,5 +136,10 @@ const signOut = () => {
     client.auth.signOut()
     userStore.isMenuOverlay = false
     return navigateTo('/')
+}
+
+const signIn = () => {
+    userStore.isMenuOverlay = false
+    return navigateTo('/auth')
 }
 </script>
