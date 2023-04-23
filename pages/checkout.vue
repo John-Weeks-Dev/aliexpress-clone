066d4a39f9717fd4a2a4a9e7d787b7f4fde3e5f4
JohnWeeks1 <jonathanweeks007@gmail.com>
2023-04-23 15:38:51 +0700

    first commit

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -1,32 +1,8 @@
 <template>
     <MainLayout>
         <div id="CheckoutPage" class="mt-4 max-w-[1200px] mx-auto px-2">
-            <div v-if="false" class="h-[500px] flex items-center justify-center">
-                <div class="pt-20">
-                    <img 
-                        class="mx-auto"
-                        width="250"
-                        src="/cart-empty.png"
-                    >
-                    <div class="text-xl text-center mt-4">No items yet? Tap below to explore</div>
-                    <button 
-                        class="
-                            bg-[#FD374F] 
-                            w-full 
-                            text-white 
-                            text-[21px] 
-                            font-semibold 
-                            p-1.5 
-                            rounded-full
-                            mt-4
-                        "
-                    >
-                        Sign in
-                    </button>
-                </div>
-            </div>
-
-            <div v-else class="md:flex gap-4 justify-between mx-auto w-full">
+  
+            <div class="md:flex gap-4 justify-between mx-auto w-full">
                 <div class="md:w-[65%]">
                     <div class="bg-white rounded-lg p-4">
 
